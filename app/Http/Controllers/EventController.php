90bcce2e79f69fb1ca1df35a9f49277ffbb19086
Your Name <you@example.com>
2020-12-06 15:55:12 -0300

    Create events and flash msg finished

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -19,6 +19,24 @@ class EventController extends Controller
         return view('events.create');
     }
 
+    public function store(Request $request){
+
+        $event = new Event();
+
+        $event->title = $request->title;
+        $event->city = $request->city;
+        $event->private = $request->private;
+        $event->description = $request->description;
+
+        if ($event->title == "" || $event->city == "" || $event->private == "" || $event->description == "") {
+            return redirect('/events/create')->with('msg', 'Favor preencher os campos');
+        }
+
+        $event->save();
+
+        return redirect('/')->with('msg', 'Evento criado com sucesso!');
+    }
+
     public function login(){
         return view('events.login');
     }
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -45,8 +45,17 @@
             </div>
         </nav>
     </header>
+    <main>
+        <div class="container-fluid">
+            <div class="row">
+                @if (session('msg'))
+                    <p class="msg">{{ session('msg') }}</p>
+                @endif
+                @yield('content')
+            </div>
+        </div>
+    </main>
 
-    @yield('content')
 
     <footer>
         <p>Laravel events</p>
